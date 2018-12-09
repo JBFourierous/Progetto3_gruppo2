38453874bf787d314117d8e5f8f8151001922952
bkoala42 <ilariagigi@virgilio.it>
2018-12-09 11:33:43 +0100

    structure

---
+++ pkg_4/exercise_4.py
@@ -0,0 +1,12 @@
+from TdP_collections.graphs.graph import Graph
+
+
+def bipartite(G: Graph):
+    """
+    La funzione verifica se G è bipartito e restituisce una partizione (X, Y) dei vertici di G tale che
+    tutti gli archi del grafo collegano un vertice di X ad un vertice di Y. Nel caso in cui il grafo non
+    sia bipartito la funzione deve restituire None.
+    :param G: grafo non diretto
+    :return:
+    """
+    pass
