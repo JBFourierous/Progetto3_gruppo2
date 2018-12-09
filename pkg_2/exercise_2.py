fe2523f2dc624e884a28ea97f39f9da37cf1ab22
bkoala42 <ilariagigi@virgilio.it>
2018-12-09 11:15:23 +0100

    structure of function

---
+++ pkg_2/exercise_2.py
@@ -0,0 +1,17 @@
+from pkg_1.exercise_1 import Route, Schedule
+import datetime
+
+
+def find_route(s: Schedule, a: str, b: str, t: datetime.time) -> Route:
+    """
+    La funzione trova la rotta che permette di arrivare da a a b nel minor tempo
+    possibile, partendo ad un orario non precedente a t. (Come per l’esercizio
+    precedente, bisogna tener conto del tempo minimo di coincidenza di ogni
+    scalo).
+    :param s: orario della compagnia
+    :param a: areoporto di partenza
+    :param b: areoporto di arrivo
+    :param t: orario di partenza
+    :return: la rotta che rispetta i vincoli imposti
+    """
+    pass
