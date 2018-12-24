44372ae752e34e7ad76293b4d404f267ce97cb30
bkoala42 <ilariagigi@virgilio.it>
2018-12-24 12:20:01 +0100

    complete test

--- pkg_1/test_exercise_1.py
+++ pkg_1/test_exercise_1.py
@@ -7,12 +7,6 @@ from pkg_1.utils import l, a
 def test_paths_available():
     airports, schedule = initialize_schedule("../airports.txt", "../flights.txt")
 
-    # for airport in airports:
-    #     print(airport)
-    # for airport, listofflights in schedule.items():
-    #     for flight in listofflights:
-    #         print(airport, "flight: ", flight)
-
     start = airports[0]
     end = airports[1]
 
@@ -21,30 +15,52 @@ def test_paths_available():
 
     paths = list_routes(schedule, start, end, start_time, total_time)
 
-    # the result is correct if
-    # time the flight start - time you arrive + length of the flight < total_time
-
-    # print("\nI want to go from " + str(start.getName()) + " to " + str(end.getName()) +
-    #       " starting at " + str(start_time) + " in max " + str(total_time))
-    cost = None
+    list_costs = list()
     if len(paths) != 0:
         for path in paths:
             cost = timedelta(0)
             arrive_time = start_time
             for f in path:
                 curr_cost = l(f) - arrive_time + a(f) - l(f)
-                # print(curr_cost)
                 cost = curr_cost + cost
                 arrive_time = a(f)
-                # print(f)
-            # print(cost)
             if cost > total_time:
                 print("Test test_paths_available failed")
                 return
-        if len(paths) == 5 and cost < total_time:
+            list_costs.append(cost)
+        if len(paths) == 5 and len(list_costs) == 5 and all(i <= total_time for i in list_costs):
             print("Test test_paths_available passed")
     else:
         print("Test test_paths_available failed")
+
+
+def test_other_paths_available():
+    airports, schedule = initialize_schedule("../airports.txt", "../flights.txt")
+
+    start = airports[6]  # FCO
+    end = airports[0]    # LHR
+
+    start_time = timedelta(hours=6, minutes=00)
+    total_time = timedelta(hours=10)
+    paths = list_routes(schedule, start, end, start_time, total_time)
+
+    list_costs = list()
+    if len(paths) != 0:
+        for path in paths:
+            cost = timedelta(0)
+            arrive_time = start_time
+            for f in path:
+                curr_cost = l(f) - arrive_time + a(f) - l(f)
+                cost = curr_cost + cost
+                arrive_time = a(f)
+            if cost > total_time:
+                print("Test test_paths_available failed")
+                return
+            list_costs.append(cost)
+        if len(paths) == 9 and len(list_costs) == 9 and all(i <= total_time for i in list_costs):
+            print("Test test_other_paths_available passed")
+    else:
+        print("Test test_other_paths_available failed")
 
 
 def test_no_flight_available():
@@ -64,6 +80,7 @@ def test_no_flight_available():
 
 def execute_tests():
     test_paths_available()
+    test_other_paths_available()
     test_no_flight_available()
 
 
--- pkg_2/test_exercize2.py
+++ pkg_2/test_exercize2.py
@@ -1,33 +1,113 @@
 from pkg_1.utils import initialize_schedule
 from datetime import timedelta
 from pkg_2.exercise_2 import find_route
+from pkg_1.exercise_1 import list_routes
+from pkg_1.utils import l, a
 
-if __name__ == "__main__":
+
+def test_path_available_check_it_is_the_shortest():
     airports, schedule = initialize_schedule("../airports.txt", "../flights.txt")
-
-    # for airport in airports:
-    #     print(airport)
-    # for airport, list_of_flights in schedule.items():
-    #     for flight in list_of_flights:
-    #         print(airport, "flight: ", flight)
 
     start = airports[0]
     end = airports[1]
 
-    start_time = timedelta(hours=6, minutes=40)
-    total_time = timedelta(hours=10)
+    start_time = timedelta(hours=12, minutes=00)
+
+    path = find_route(schedule=schedule,
+                      airports=airports,
+                      start=start,
+                      dest=end,
+                      t=start_time)
+
+    cost = None
+    if len(path) != 0:
+        cost = timedelta(0)
+        arrive_time = start_time
+        for f in path:
+            curr_cost = l(f) - arrive_time + a(f) - l(f)
+            cost = curr_cost + cost
+            arrive_time = a(f)
+    else:
+        print("Test test_path_available_check_it_is_the_shortest failed")
+
+    # Chiamando la list_routes() dell'esercizio 1 richiedendo come costo totale
+    # il costo dello shortest path, la funzione deve restituire solo e soltando quel volo.
+    paths = list_routes(schedule=schedule,
+                        source=start,
+                        dest=end,
+                        t=start_time,
+                        T=cost)
+    if len(path) != 1:
+        print("Test test_path_available_check_it_is_the_shortest failed")
+    else:
+        if str(paths[0][0]) == str(path[0]):
+            print("Test test_path_available_check_it_is_the_shortest passed")
+        else:
+            print("Test test_path_available_check_it_is_the_shortest failed")
+
+
+def test_another_path_available_check_it_is_the_shortest():
+    airports, schedule = initialize_schedule("../airports.txt", "../flights.txt")
+
+    start = airports[6]  # FCO
+    end = airports[0]  # LHR
+
+    start_time = timedelta(hours=6, minutes=00)
+
+    path = find_route(schedule=schedule,
+                      airports=airports,
+                      start=start,
+                      dest=end,
+                      t=start_time)
+
+    cost = None
+    if len(path) != 0:
+        cost = timedelta(0)
+        arrive_time = start_time
+        for f in path:
+            curr_cost = l(f) - arrive_time + a(f) - l(f)
+            cost = curr_cost + cost
+            arrive_time = a(f)
+    else:
+        print("Test test_another_path_available_check_it_is_the_shortest failed")
+
+    # Chiamando la list_routes() dell'esercizio 1 richiedendo come costo totale
+    # il costo dello shortest path, la funzione deve restituire solo e soltando quel volo.
+    paths = list_routes(schedule=schedule,
+                        source=start,
+                        dest=end,
+                        t=start_time,
+                        T=cost)
+    if all(str(paths[0][i]) == str(path[i]) for i in range(len(path))) and \
+            len(path) == len(paths[0]):
+        print("Test test_another_path_available_check_it_is_the_shortest passed")
+    else:
+        print("Test test_another_path_available_check_it_is_the_shortest failed")
+
+
+def test_no_flight_available():
+    airports, schedule = initialize_schedule("../airports.txt", "../flights.txt")
+    start = airports[8]  # OSL
+    end = airports[7]    # SVO
+
+    start_time = timedelta(hours=8, minutes=00)
 
     paths = find_route(schedule=schedule,
                        airports=airports,
                        start=start,
                        dest=end,
                        t=start_time)
-    print("\nI want to go from " + str(start.getName()) + " to " + str(end.getName()) +
-          " starting at " + str(start_time) + " in the shortest time possible ")
     if len(paths) != 0:
-        for path in paths:
-            print("--------------PATH----------- ")
-            print(path)
+        print("Test test_no_flight_available failed")
     else:
-        print("No flight available")
+        print("Test test_no_flight_available passed")
 
+
+def execute_test():
+    test_path_available_check_it_is_the_shortest()
+    test_another_path_available_check_it_is_the_shortest()
+    test_no_flight_available()
+
+
+if __name__ == "__main__":
+    execute_test()
