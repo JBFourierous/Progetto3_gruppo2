142e295352472c2be06a390c8bc9bc9fea1926f8
bkoala42 <ilariagigi@virgilio.it>
2018-12-26 12:41:50 +0100

    update doc

--- pkg_2/exercise_2.py
+++ pkg_2/exercise_2.py
@@ -2,6 +2,7 @@ from TdP_collections.priority_queue.adaptable_heap_priority_queue import Adaptab
 from typing import Dict
 from pkg_1.utils import Airport, List, d, l, a, c, s
 from datetime import timedelta
+from collections import deque
 
 
 def find_route(schedule: Dict, airports: List, start: Airport, dest: Airport, t: timedelta) -> List:
@@ -19,7 +20,7 @@ def find_route(schedule: Dict, airports: List, start: Airport, dest: Airport, t:
     Complessità computazionale O((n+m) log n), ma si può ragionevolmente assumere che
     m >> n, per cui O(m log n), in cui n è il numero di aereoporti ed m il numero di voli
     """
-    path = list()                           # insieme dei voli che costituiscono il percorso più breve
+    path = deque()                          # insieme dei voli che costituiscono il percorso più breve
     q = AdaptableHeapPriorityQueue()        # coda a priorità per Dijkstra
     cloud = dict()                          # insieme di nodi visitati da Dijkstra con loro costo
     costs = dict()                          # dizionario dei costi ottimi valutati da Dijkstra
@@ -62,13 +63,14 @@ def find_route(schedule: Dict, airports: List, start: Airport, dest: Airport, t:
                         # la modifica della key di un elemento richiede tempo O(log n)
                         q.update(locators[d(flight)], costs[d(flight)], (d(flight), flight, a(flight)))
 
-    # ripercorro all'indietro i voli che mi portano a destinazione
-    # O(m^2)
+    # ripercorro all'indietro i voli che mi portano a destinazione per costruire la rotta della soluzione
+    # si utilizza una deque (double-ended queue) che è rappresentata internamente come una lista doppiamente linkata,
+    # in modo da avere l'inserimento in testa più efficiente possibile e cioè O(1)
+    # nel caso peggiore potrebbero essere inseriti m elementi, con complessità totale O(m)
     flight_to_take = cloud[dest]
     while flight_to_take is not None:
-        # costruisco la rotta della soluzione, ogni insert in una lista built-in richiede tempo O(m)
-        path.insert(0, flight_to_take)
+        path.appendleft(flight_to_take)
         flight_to_take = cloud[s(flight_to_take)]
-    return path
-
+    # cast a lista richiede O(n) con n numero di elementi nella collezione
+    return list(path)
 
