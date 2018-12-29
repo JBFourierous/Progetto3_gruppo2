0baa15de58f066dbac81f45be0e4ac135dc864ca
JBFourierous <marco.carpentiero@gmail.com>
2018-12-29 15:49:05 +0100

    ...

--- pkg_3/test_exercise3.py
+++ pkg_3/test_exercise3.py
@@ -4,13 +4,13 @@ from pkg_3.exercise_3 import select_flight
 def test_select_flight_result_is_the_expected_one():
     airports, flights = initialize_schedule("../airports.txt", "../flights_light.txt")
     result, money, total_cost = select_flight(airports, flights, 600)
-    if str(result[0]) == "BCN FCO 18:25:00 20:15:00 517"\
-            and str(result[1]) == "BCN FCO 16:40:00 18:25:00 625"\
-            and str(result[2]) == "LHR FCO 16:10:00 19:45:00 651"\
+    if str(result[0]) == "BCN FCO 2018/12/29 18:25:00 20:15:00 517"\
+            and str(result[1]) == "BCN FCO 2018/12/29 16:40:00 18:25:00 625"\
+            and str(result[2]) == "LHR FCO 2018/12/29 16:10:00 19:45:00 651"\
             and total_cost == 550:
         print("test passed")
     else:
-        print("test_failed")
+        print("test failed")
 
 def test_select_flight_result_is_the_expected_one_B_lessequal_zero():
     airports, flights = initialize_schedule("../airports.txt", "../flights_light.txt")
